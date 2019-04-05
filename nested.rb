af5064b8982acfce58740adcceaa707656154cee
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-05 01:45:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,9 +86,11 @@ def adding_matz
      }
 
 
-programmer_hash[:yukihiro_matsumoto]
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash.merge(:yukihiro_matsumoto => {
+	:known_for => "Ruby"
+	:languages => ["LISP", "C"]
+} )
+
 
 
 end
