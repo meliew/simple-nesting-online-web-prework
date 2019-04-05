f54e1fcf01cb663a446ebb54c4380df949d1a25b
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-05 01:10:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:known_for] = new_hash.to_s
+programmer_hash[:dennis_ritchie][:known_for].to_s
 
 def adding_matz
 # add the following information to the top level of programmer_hash
